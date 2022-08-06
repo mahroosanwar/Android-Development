c3087f5b0106056dfaed2f36a74bf141580da0a1
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-06 19:40:01 +0530

    Create variable2.kt

---
+++ Kotlin/freeCodeCampKotlin/variable2.kt
@@ -0,0 +1,15 @@
+//Variable 2
+
+val name = "Mahroos"
+var greeting: String? = null
+
+
+fun main() {
+//     greeting = "hello"
+    if(greeting != null) {
+        println(greeting)
+    } else {
+        println("Hi")
+    }
+    println(name)
+}
