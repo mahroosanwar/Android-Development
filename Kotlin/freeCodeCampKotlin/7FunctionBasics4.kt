9f2967d7f8132dda4771b12a81ed3ec64082dc5e
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-07 10:35:21 +0530

    Create 7FunctionBasics4.kt

---
+++ Kotlin/freeCodeCampKotlin/7FunctionBasics4.kt
@@ -0,0 +1,7 @@
+fun sayHello(greeting: String, itemToGreet: String) = println("$greeting $itemToGreet")
+
+fun main() {
+    sayHello(greeting = "Hello", itemToGreet = "Kotlin")
+    sayHello(greeting = "Hi", itemToGreet = "Mahroos")
+
+}
