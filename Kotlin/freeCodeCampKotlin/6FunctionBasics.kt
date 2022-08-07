e2b571f61a9a3fe50acb0f4dab48ae57b9235a9d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-07 10:27:36 +0530

    Create 6FunctionBasics.kt

---
+++ Kotlin/freeCodeCampKotlin/6FunctionBasics.kt
@@ -0,0 +1,11 @@
+// Function Basics 2
+
+fun sayHello(itemToGreet: String) {
+    val msg = "Hello " + itemToGreet
+    println(msg)
+}
+
+fun main() {
+    sayHello(itemToGreet = "Kotlin")
+    sayHello(itemToGreet = "Mahroos")
+}
