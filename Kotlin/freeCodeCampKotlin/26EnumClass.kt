30ecfa8443c91d07de98ec7266c250bdd52a7c95
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-10 02:29:16 +0530

    Create 26EnumClass.kt

---
+++ Kotlin/freeCodeCampKotlin/26EnumClass.kt
@@ -0,0 +1,42 @@
+import java.util.UUID
+
+//Enum Classes
+enum class EntityType{
+    EASY, MEDIUM, HARD;
+
+    fun getFormattedName() = name.lowercase()
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+}
+
+object EntityFactory {
+
+    fun create(type: EntityType) : Entity {
+
+        val id = UUID.randomUUID().toString()
+
+        val name: when(type) {
+            EntityType.EASY -> type.name
+            EntityType.MEDIUM -> type.getFormattedName()
+            EntityType.HARD -> "Hard
+        }
+
+        return Entity(id, name = "name")
+    }
+
+}
+class Entity (val id: String, val name: String) {
+    override fun toString(): String {
+        return "id:$id name: $name"
+    }
+}
+
+fun main() {
+//    val entity = Entity(id:"id")  /*error: can't access privater to entity, so we have to use companion object */
+
+    val entity = EntityFactory.create()
+    println(entity)
+
+    val mediumEntity = EntityFactory.create(EntityType.MEDIUM)
+
+    println(mediumEntity)
+}
